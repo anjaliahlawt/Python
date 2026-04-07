6bc9247b1d96ca9f2c3dec4db51843fb1b853a00
Anjali <anjaliahlawat45l@gmail.com>
2026-04-07 12:01:39 +0530

    added constructor

--- oops.py
+++ oops.py
@@ -14,12 +14,38 @@
 
 #method 2
 
-class Student:
-    name="anjali"
-    age=20
-    grade="B"
-    def display(self):
-        print(self.name,self.age,self.grade)
+# class Student:
+#     name="anjali"
+#     age=20
+#     grade="B"
+#     def display(self):
+#         print(self.name,self.age,self.grade)
 
-s1= Student()
-s1.display()
+# s1= Student()
+# s1.display()
+
+
+#construtor:is a special method that is automatically called when an object of a class is created. It is used to initialize the attributes of the object.
+
+# class Student:
+#     def __init__(self):
+#         print("constructor call")
+
+# s1=Student()
+
+
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+s1=student("anju",20)
+print(s1.name)
+
+#without contsructor
+
+class Student:
+    pass
+
+s1 = Student()
+s1.name = "Anjali"   # manually dena padta hai 
